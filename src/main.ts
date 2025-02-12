800199a5f003195df00418064a7c1460dfbe1156
giacomotolari <tolarigiacomo@gmail.com>
2025-02-12 10:05:29 +0100

    update index.html to add a weather search form and modify heading; enhance main.ts to handle form submission

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,12 @@
-import './style.css'
+import "./style.css";
 
+const formEl = document.querySelector("form");
+
+function getCity(event: Event) {
+  event.preventDefault();
+  const formData = new FormData(event.target as HTMLFormElement);
+  const formInputs = Object.fromEntries(formData.entries());
+  const cityValue = formInputs.city;
+}
+
+formEl?.addEventListener("submit", getCity);
