ca6d7fa4fdfb238e9b58c9352c64222754cb95f7
giacomotolari <tolarigiacomo@gmail.com>
2025-02-12 10:33:23 +0100

    feat: add input validation and sanitizing

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ function getCity(event: Event) {
   event.preventDefault();
   const formData = new FormData(event.target as HTMLFormElement);
   const formInputs = Object.fromEntries(formData.entries());
-  const cityValue = formInputs.city;
+  const cityValue = (formInputs.city as string).trim().toLowerCase();
+  const isCityValid =
+    cityValue.length >= 2 && cityValue.length <= 100 ? "valid" : "invalid";
+  console.log("🚀 ~ getCity ~ cityValue:", cityValue)
+  console.log("🚀 ~ getCity ~ isCityValid:", isCityValid);
 }
 
 formEl?.addEventListener("submit", getCity);
